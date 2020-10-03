cfc2269b24069eb4510b8f64a08604c945b88677
Juan Camilo Guzman <jcamiloguz1@gmail.com>
2020-10-02 23:34:34 -0500

    Addin CORS config

--- routes.go
+++ routes.go
@@ -9,14 +9,26 @@ import (
 	"github.com/dgraph-io/dgo/protos/api"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 )
 
 func Route() *chi.Mux {
 	mux := chi.NewMux()
+	cors := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
 	//Globals middlewares
 	mux.Use(
 		middleware.Logger,
 		middleware.Recoverer,
+		cors.Handler,
 	)
 	mux.Get("/buyer", getBuyersHandler)
 	mux.Post("/buyer", postBuyersHandler)
@@ -40,12 +52,7 @@ func getBuyersHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	var buyers Buyer
-	if err := json.Unmarshal(resp.GetJson(), &buyers); err != nil {
-		log.Fatal(err)
-	}
-
+	w.Write(resp.Json)
 }
 
 func postBuyersHandler(w http.ResponseWriter, r *http.Request) {
